2abc5f1cd193057cd56a6cb4dccd47c2ff06e9b1
Aleksey Demchenko <al.demchenko34@gmail.com>
2022-11-26 01:49:55 +0300

    Add graphic module, check_class function and Docstrings

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+"""Text Game."""
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """
+    Сreate the amount of damage inflicted
+    for each classes of heroes.
+    """
     if char_class == 'warrior':
         warrior_damage = 5 + randint(3, 5)
         return (f'{char_name} нанёс урон противнику '
                 f'равный {warrior_damage}')
     if char_class == 'mage':
         mage_damage = 5 + randint(5, 10)
-        return (f'{char_name} нанёс урон противнику'
+        return (f'{char_name} нанёс урон противнику '
                 f'равный {mage_damage}')
     if char_class == 'healer':
         healer_damage = 5 + randint(-3, -1)
-        return (f'{char_name} нанёс урон противнику'
+        return (f'{char_name} нанёс урон противнику '
                 f'равный {healer_damage}')
-    return ' '
+    return 'Choose correct class'
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """
+    Сreate determines the number of blocked damage
+    for each classes of heroes.
+    """
     if char_class == 'warrior':
         warrior_defence = 10 + randint(5, 10)
         return (f'{char_name} блокировал {warrior_defence} урона')
@@ -27,10 +38,11 @@ def defence(char_name: str, char_class: str) -> str:
     if char_class == 'healer':
         healer_defence = 10 + randint(2, 5)
         return (f'{char_name} блокировал {healer_defence} урона')
-    return ' '
+    return 'Choose correct class'
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Сreate the special skill for each classes of heroes."""
     if char_class == 'warrior':
         warrior_skill = 80 + 25
         return (f'{char_name} применил специальное умение '
@@ -43,16 +55,19 @@ def special(char_name: str, char_class: str) -> str:
         healer_skill = 10 + 30
         return (f'{char_name} применил специальное '
                 f'умение «Защита {healer_skill}»')
-    return ' '
+    return 'Choose correct class'
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Сreate the gameplay of the game."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
         print(f'{char_name}, ты Маг — превосходный укротитель стихий.')
     if char_class == 'healer':
         print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
+    if char_class not in ('warrior','mage','healer'):
+        print(f'{char_name}, выбирете персонажа из предложенного списка')
     print('Потренируйся управлять своими навыками.')
     print('Введи одну из команд: attack — чтобы атаковать противника, '
           'defence — чтобы блокировать атаку противника'
@@ -71,6 +86,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Сharacter selection and description function."""
     approve_choice: str = ' '
     char_class: str = ' '
     while approve_choice != 'y':
@@ -92,16 +108,21 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+def check_class(char_name: str, char_class: str) -> str:
+    """Check the correctness of the input"""
+    if char_class in ('warrior','mage','healer'):
+        return start_training(char_name, char_class)
+    return 'Выберите класс из предложенного списка'
+
+
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
-    print(start_training(char_name, char_class))
-
-
-main()
+    char_class: str = choice_char_class()
+    print(check_class(char_name, char_class))
